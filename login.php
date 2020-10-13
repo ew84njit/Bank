f667ca2668c6f3cfce568465d9d085c7b592f953
ewangso <skyromex@gmail.com>
2020-10-12 20:15:40 -0400

    test change 6

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if (isset($_POST["login"])) {
         require_once(__DIR__."/lib/db.php");
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
+            $stmt = $db->prepare("SELECT id, email, username, password from BankUsers WHERE email = :email LIMIT 1");
 
             $params = array(":email" => $email);
             $r = $stmt->execute($params);
