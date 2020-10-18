58257029d7107615c24dbaf10165a1c3249aed46
ewangso <skyromex@gmail.com>
2020-10-18 17:51:06 -0400

    styling

--- partials/nav.php
+++ partials/nav.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html>
 <?php
-//we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 
-<head>
-<link rel = "stylesheet" type="text/css" href="theme.css" media="all"/>
-</head>
+<style>
+<?php include 'theme.css'; ?>
+</style>
 
 <body>
     <div class="navbar">
-
         <a class="navItem" href="home.php">Home</a>
         <?php if (!is_logged_in()): ?>
             <a class="navItem" href="login.php">Login</a>
@@ -21,7 +19,6 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <a class="navItem" href="profile.php">Profile</a>
             <a class="navItem" href="logout.php">aLogout</a>
         <?php endif; ?>
-
     </div>
 </body>
 
