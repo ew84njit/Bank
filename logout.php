8b139f557bd3f15e377e6417fa051488e7a5a9ba
ewangso <skyromex@gmail.com>
2020-10-18 19:19:19 -0400

    logout fix

--- logout.php
+++ logout.php
@@ -4,8 +4,8 @@ session_unset();
 session_destroy();
 ?>
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<script>alert("You are logged out");</script>
+
 <?php
-echo "You're logged out now";
+echo '<script>alert("You are logged out")</script>'; 
 header("Location: login.php");
 ?>
