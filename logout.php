5bab726b4e8c54597f5de6978c58fb82cdd35829
ewangso <skyromex@gmail.com>
2020-10-18 19:05:32 -0400

    logout fix

--- logout.php
+++ logout.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
-// remove all session variables
 session_unset();
-// destroy the session
 session_destroy();
 ?>
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-echo "You're logged out (proof by dumping the session)<br>";
-echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+echo "You're logged out now";
+header("Location: login.php");
 ?>
