2f275048c4adaa810b718509e1ea70e901dbe56c
ewangso <skyromex@gmail.com>
2020-10-18 17:48:45 -0400

    styling

--- partials/nav.php
+++ partials/nav.php
@@ -6,23 +6,23 @@ require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 
 <head>
-<link rel = "stylesheet" type="text/css" href = "theme.css" media="all"/>
+<link rel = "stylesheet" type="text/css" href="theme.css" media="all"/>
 </head>
 
 <body>
-<div class="navbar">
-    <ul>
-        <li><a class="navItem" href="home.php">Home</a></li>
+    <div class="navbar">
+
+        <a class="navItem" href="home.php">Home</a>
         <?php if (!is_logged_in()): ?>
-            <li><a class="navItem" href="login.php">aLogin</a></li>
-            <li><a class="navItem" href="register.php">Register</a></li>
+            <a class="navItem" href="login.php">Login</a>
+            <a class="navItem" href="register.php">Register</a>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
-            <li><a class="navItem" href="profile.php">Profile</a></li>
-            <li><a class="navItem" href="logout.php">aLogout</a></li>
+            <a class="navItem" href="profile.php">Profile</a>
+            <a class="navItem" href="logout.php">aLogout</a>
         <?php endif; ?>
-    </ul>
-</div>
+
+    </div>
 </body>
 
 </html>
