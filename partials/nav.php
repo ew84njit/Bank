08a873a75f249012ede9c541f0c504e186d1b37a
ewangso <skyromex@gmail.com>
2020-11-12 11:41:08 -0500

    view and list egg

--- partials/nav.php
+++ partials/nav.php
@@ -14,6 +14,10 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <a class="navItem" href="login.php">Login</a>
         <a class="navItem" href="register.php">Register</a>
     <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+        <li><a href="test_create_egg.php">Create Egg</a></li>
+        <li><a href="test_list_egg.php">View Eggs</a></li>
+    <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <a class="navItem" href="profile.php">Profile</a>
         <a class="navItem" href="logout.php">Logout</a>
