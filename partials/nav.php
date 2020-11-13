7ebd50c515877dc8951ec1912eb5dc7240199fea
ewangso <skyromex@gmail.com>
2020-11-12 20:55:08 -0500

    edit accounts

--- partials/nav.php
+++ partials/nav.php
@@ -15,8 +15,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <a class="navItem" href="register.php">Register</a>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-        <li><a href="test_create_egg.php">Create Egg</a></li>
-        <li><a href="test_list_egg.php">View Eggs</a></li>
+        <li><a href="test_create_accounts.php">Create Account</a></li>
+        <li><a href="test_list_accounts.php">View Eggs</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <a class="navItem" href="profile.php">Profile</a>
