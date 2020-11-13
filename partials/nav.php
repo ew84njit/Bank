27e53e29cf4dad1e117ca851e46431be40a55e92
ewangso <skyromex@gmail.com>
2020-11-12 21:13:44 -0500

    changed nav

--- partials/nav.php
+++ partials/nav.php
@@ -16,7 +16,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
         <li><a href="test_create_accounts.php">Create Account</a></li>
-        <li><a href="test_list_accounts.php">View Eggs</a></li>
+        <li><a href="test_list_accounts.php">View Account</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <a class="navItem" href="profile.php">Profile</a>
