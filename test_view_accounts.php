958c6fa4603a4e42b26230313dabfb546cd88d6c
ewangso <skyromex@gmail.com>
2020-11-13 21:34:08 -0500

    test

--- test_view_accounts.php
+++ test_view_accounts.php
@@ -17,7 +17,7 @@ if (isset($_GET["id"])) {
 $result = [];
 if (isset($id)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT Eggs.id,name,state,base_rate,mod_min,mod_max,next_stage_time, user_id, Users.username FROM F20_Eggs as Eggs JOIN Users on Eggs.user_id = Users.id where Eggs.id = :id");
+    $stmt = $db->prepare("SELECT account_number, user_id, account_type, balance FROM Accounts where id = :id");
     $r = $stmt->execute([":id" => $id]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$result) {
@@ -34,11 +34,10 @@ if (isset($id)) {
         <div class="card-body">
             <div>
                 <p>Stats</p>
-                <div>Rate: <?php safer_echo($result["base_rate"]); ?></div>
-                <div>Modifier: <?php safer_echo($result["mod_min"]); ?> - <?php safer_echo($result["mod_max"]); ?></div>
-                <div>Current State: <?php getState($result["state"]); ?></div>
-                <div>Next Stage: <?php safer_echo($result["next_stage_time"]); ?></div>
-                <div>Owned by: <?php safer_echo($result["username"]); ?></div>
+                <div>Account #: <?php safer_echo($result["account_number"]); ?></div>
+                <div>User ID: <?php safer_echo($result["user_id"]); ?></div>
+                <div>Next Stage: <?php safer_echo($result["account_type"]); ?></div>
+                <div>Owned by: <?php safer_echo($result["balance"]); ?></div>
             </div>
         </div>
     </div>
