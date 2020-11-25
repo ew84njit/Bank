ed5c4b7013d817a684c2d0dba56667027c40c92c
ewangso <skyromex@gmail.com>
2020-11-25 11:29:50 -0500

    test

--- test_view_accounts.php
+++ test_view_accounts.php
@@ -12,11 +12,12 @@ echo("get id");
 //we'll put this at the top so both php block have access to it
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
+    echo("in if");
 }
 ?>
 
 <?php
-echo($id);
+
 $result = [];
 if (isset($id)) {
     $db = getDB();
