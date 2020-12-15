cf7e0f0e4dee2cc40e1e999726b1bb1658970a41
ewangso <skyromex@gmail.com>
2020-12-15 13:34:02 -0500

    reset password

---
+++ reset_pass.php
@@ -0,0 +1,9 @@
+<form method="POST">
+    <!-- DO NOT PRELOAD PASSWORD-->
+    <label for="pw">Password</label>
+    <input type="password" name="password"/>
+    <label for="cpw">Confirm Password</label>
+    <input type="password" name="confirm"/>
+    <input type="submit" name="saved" value="Save Profile"/>
+</form>
+
